3899442cedde3f493290623161bfa576b58f7bfe
Masud8851 <masud1971.cse@gmail.com>
2021-12-18 23:55:08 +0600

    Addd

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDwfYuQa9qyWD95QfxaM0RluupN-wDfBNA",
+	authDomain: "sunglassshop-472b0.firebaseapp.com",
+	projectId: "sunglassshop-472b0",
+	storageBucket: "sunglassshop-472b0.appspot.com",
+	messagingSenderId: "710249935917",
+	appId: "1:710249935917:web:ca5db0772d7184ae5d74a4",
+};
+
+export default firebaseConfig;
